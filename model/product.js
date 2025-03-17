56fd9971087e84ac28f6e7bb0338b586ea731cea
annapurna12367 <mathadannapurana901@gmail.com>
2025-03-17 15:28:02 +0530

    done

---
+++ model/product.js
@@ -0,0 +1,35 @@
+const mongoose =require('mongoose');
+const productSchema= new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+
+
+    },
+    price:{
+        type:Number,
+        required:true
+    },
+    brand:{
+        type:String
+    },
+    brand:{
+        type:String
+    },
+    stock:{
+        type:Number
+    },
+    image:{
+        type:String
+    },
+    description:{
+    type:String
+    },
+    user:{
+        type:mongoose.Schema.ObjectId,
+        ref:'User'
+    },
+
+})
+const Product = mongoose.model('Product',productSchema);
+module.experts={Product};
